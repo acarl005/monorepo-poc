3f31121ac36f4a17c3fe340b41dd157029a291e5
Andrew Carlson <acarl005@g.ucla.edu>
2017-10-13 11:59:20 -0700

    yet another change

--- packages/a/src/index.js
+++ packages/a/src/index.js
@@ -2,5 +2,5 @@ import chalk from 'chalk'
 const { version } = require('../package.json')
 
 module.exports = () => {
-  console.log(chalk.cyan(`version ${version} of "a" says EY MAN`))
+  console.log(chalk.cyan(`version ${version} of "a" says EY BRO`))
 }
